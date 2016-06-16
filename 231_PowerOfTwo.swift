c121081498361b06bf9b4c0bd77ab86ae98d3393
Bayonetta <shuaizhao0315@gmail.com>
2016-06-16 13:44:22 +0800

    solved 231

---
+++ 231_PowerOfTwo.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+func isPowerOfTwo(n: Int) -> Bool {
+    if n == 0 {
+        return false
+    }
+    if n == 1 {
+        return true
+    }
+    if n % 2 == 0 {
+        return isPowerOfTwo(n/2)
+    }
+    return false
+}
